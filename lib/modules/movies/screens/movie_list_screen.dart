a3880003f643a3aaf8735da92fbdb420ee4c149e
Vinicius Rosa <viniciusrosarafaeldeoliveira@gmail.com>
2023-02-02 09:48:49 -0300

    improved separation of concerns

--- lib/modules/movies/screens/movie_list_screen.dart
+++ lib/modules/movies/screens/movie_list_screen.dart
@@ -1,9 +1,9 @@
 import 'package:cine_flutter/common/animations/down_top_entrance.dart';
-import 'package:cine_flutter/common/widgets/blurred_image_bg.dart';
+import 'package:cine_flutter/common/widgets/blurred_background.dart';
 import 'package:cine_flutter/modules/movies/controllers/movie_list_controller.dart';
 import 'package:cine_flutter/modules/movies/domain/models/movie.dart';
-import 'package:cine_flutter/modules/movies/widgets/movie_header.dart';
 import 'package:cine_flutter/modules/movies/widgets/movie_img_card.dart';
+import 'package:cine_flutter/modules/movies/widgets/movie_list_header.dart';
 import 'package:flutter/material.dart';
 
 class MovieListScreen extends StatelessWidget {
@@ -16,49 +16,44 @@ class MovieListScreen extends StatelessWidget {
     return AnimatedBuilder(
       animation: controller,
       builder: (context, child) {
-        return Stack(
-          children: [
-            BlurredImageBg(
-              controller.movie?.thumbUrl ?? "",
-              key: ValueKey(controller.movie?.thumbUrl),
-            ),
-            SafeArea(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  MovieHeader(key: ValueKey(controller.movie), movie: controller.movie),
-                  Expanded(
-                    child: DownTopEntrance(
-                      child: PageView.builder(
-                        onPageChanged: controller.pageChanged,
-                        controller: controller.pageController,
-                        itemCount: controller.list.length,
-                        itemBuilder: (context, index) {
-                          Movie item = controller.list[index];
-                          return AnimatedBuilder(
-                            animation: controller.pageController,
-                            child: MovieImgCard(movie: item),
-                            builder: (context, child) {
-                              double scale = 1;
+        return BlurredBackground(
+          imgUrl: controller.movie?.thumbUrl ?? "",
+          child: SafeArea(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                MovieListHeader(key: ValueKey(controller.movie), movie: controller.movie),
+                Expanded(
+                  child: DownTopEntrance(
+                    child: PageView.builder(
+                      onPageChanged: controller.pageChanged,
+                      controller: controller.pageController,
+                      itemCount: controller.list.length,
+                      itemBuilder: (context, index) {
+                        Movie item = controller.list[index];
+                        return AnimatedBuilder(
+                          animation: controller.pageController,
+                          child: MovieImgCard(movie: item),
+                          builder: (context, child) {
+                            double scale = 1;
 
-                              if (controller.pageController.position.haveDimensions) {
-                                var factor = index.toDouble() - (controller.pageController.page ?? 0);
-                                scale = (scale - factor).clamp(0, 1);
-                              }
-                              return Transform.scale(
-                                scale: scale,
-                                child: child!,
-                              );
-                            },
-                          );
-                        },
-                      ),
+                            if (controller.pageController.position.haveDimensions) {
+                              var factor = index.toDouble() - (controller.pageController.page ?? 0);
+                              scale = (scale - factor).clamp(0, 1);
+                            }
+                            return Transform.scale(
+                              scale: scale,
+                              child: child!,
+                            );
+                          },
+                        );
+                      },
                     ),
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
-          ],
+          ),
         );
       },
     );
--- lib/modules/movies/widgets/movie_header.dart
+++ lib/modules/movies/widgets/movie_header.dart
@@ -33,75 +33,48 @@ class MovieHeader extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(left: 20, top: 20),
-      child: Row(
+      child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              DownTopEntrance(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Heading(movie?.title ?? ""),
-                    const SizedBox(height: 4),
-                    Row(
-                      children: [
-                        for (Category category in (movie?.categories ?? []))
-                          Padding(
-                            padding: const EdgeInsets.only(right: 4.0),
-                            child: CategoryTag(category: category),
-                          ),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-              const SizedBox(height: 8),
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  const Icon(
-                    Ionicons.star,
-                    size: 14,
-                    color: Colors.amber,
-                  ),
-                  const SizedBox(width: 4),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 2.5),
-                    child: Row(
-                      children: [
-                        Heading.small(rate, fontSize: 12),
-                        const Heading.small(" - ", fontSize: 12),
-                        Heading.small(totalVotes, fontSize: 12),
-                      ],
-                    ),
-                  ),
-                ],
-              )
-            ],
-          ),
-          Transform.translate(
-            offset: const Offset(-10, -10),
-            child: Row(
+          DownTopEntrance(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                IconButton(
-                  onPressed: () => print("search"),
-                  icon: const Icon(
-                    Ionicons.search_outline,
-                    color: Colors.white,
-                  ),
-                ),
-                IconButton(
-                  onPressed: () => print("notifications"),
-                  icon: const Icon(
-                    Ionicons.notifications_outline,
-                    color: Colors.white,
-                  ),
+                Heading(movie?.title ?? ""),
+                const SizedBox(height: 4),
+                Row(
+                  children: [
+                    for (Category category in (movie?.categories ?? []))
+                      Padding(
+                        padding: const EdgeInsets.only(right: 4.0),
+                        child: CategoryTag(category: category),
+                      ),
+                  ],
                 ),
               ],
             ),
+          ),
+          const SizedBox(height: 8),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Icon(
+                Ionicons.star,
+                size: 14,
+                color: Colors.amber,
+              ),
+              const SizedBox(width: 4),
+              Padding(
+                padding: const EdgeInsets.only(top: 2.5),
+                child: Row(
+                  children: [
+                    Heading.small(rate, fontSize: 12),
+                    const Heading.small(" - ", fontSize: 12),
+                    Heading.small(totalVotes, fontSize: 12),
+                  ],
+                ),
+              ),
+            ],
           )
         ],
       ),
